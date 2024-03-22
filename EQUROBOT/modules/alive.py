77e431437c205a965703a20a8a87823f1f98dd59
ᴵ'ᵐ𝗖𝗢𝗗𝗘𝗥⋆─፝─™ <164161601+TEAM-CODERZZZ@users.noreply.github.com>
2024-03-22 04:37:19 -0700

    Update alive.py

--- EQUROBOT/modules/alive.py
+++ EQUROBOT/modules/alive.py
@@ -4,4 +4,4 @@ from EQUROBOT import app
 # ---------------------------
 @app.on_message(filters.command("alive", prefixes="."))
 def alive(_, message):
-    message.reply_text("**🥀 I Aᴍ Aʟɪᴠᴇ Mʏ Dᴇᴀʀ Gᴇɴɪᴜs Mᴀsᴛᴇʀ ✨ ...**")
+    message.reply_text("**🥀 𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐌𝐲 𝐃𝐞𝐚𝐫 𝐆𝐞𝐧𝐢𝐮𝐬 𝐌𝐚𝐬𝐭𝐞𝐫 💗 ...**")
